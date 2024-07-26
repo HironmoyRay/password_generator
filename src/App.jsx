7bb753bfecee030094bb0d2e06ea516ec834e5c5
hironmoy-ray <hpray35@gmail.com>
2024-07-26 22:31:39 +0600

    useCallback & Basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [length, setLength] = useState(8);
-  const [nmAllowed, setNmAllowed] = useState(false);
+  const [passLength, setPassLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setcharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passGen = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "`~!@#$%^&*()[]{}+=.,";
+
+    for (let i = 1; i <= passLength; i++) {
+      let randomIdx = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(randomIdx);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
   return (
     <>
-      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-white bg-gray-700">
+        <h1 className="text-4xl text-center text-white my-3">
+          Password Generator
+        </h1>
+        <div className="flex shadow rounded-lg mb-4 overflow-hidden">
+          <input
+            type="text"
+            value={password}
+            placeholder="Password"
+            className="outline-none w-full py-1 px-3"
+            readOnly
+          />
+        </div>
+      </div>
     </>
   );
 }
